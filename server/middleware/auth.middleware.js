ce1c75724fdf3f2636fa8494ac41d6ad1eeeb4c0
D-1-Shetty <dioneshetty@gmail.com>
2026-06-30 03:31:30 +0530

    feat(auth): verify jwt in authentication middleware

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -4,13 +4,23 @@ import { JWT_SECRET } from "../config/env.js";
 export const protect = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-
-    if (!authHeader) {
+    
+   if (!authHeader.startsWith("Bearer ")) {
+  return res.status(401).json({
+    success: false,
+    message: "Invalid authorization format",
+  });
+}
+    
+   const token= authHeader.split(" ")[1];
+     if (!token) {
       return res.status(401).json({
         success: false,
-        message: "Unauthorized",
+        message: "Token not found",
       });
     }
+    const decoded = jwt.verify(token, JWT_SECRET);
+    console.log(decoded)
 
     next();
   } catch (error) {
