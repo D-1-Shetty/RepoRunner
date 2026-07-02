a26a152017a5bc437a8206dedd5c0844b6a7a370
D-1-Shetty <dioneshetty@gmail.com>
2026-07-02 20:56:02 +0530

    feat(git): add git clone service

--- server/services/github.service.js
+++ server/services/github.service.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-
+import { exec } from "child_process";
+import { promisify } from "util";
 export const getRepository = async (owner, repo) => {
     try{
       console.log(`https://api.github.com/repos/${owner}/${repo}`)
@@ -12,4 +13,11 @@ export const getRepository = async (owner, repo) => {
 }catch{
     return null;
 }
+};
+
+
+const execAsync = promisify(exec);
+
+export const cloneRepository = async (cloneUrl, destination) => {
+  await execAsync(`git clone ${cloneUrl} ${destination}`);
 };
